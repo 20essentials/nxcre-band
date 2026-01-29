3aa3467540415c88b1c493501093184fd3c275e0
DominicDev <s0l0estudi4r@gmail.com>
2026-01-29 16:42:35 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'NXCRE';
+export const nameSongs = [
+	"USURPER", /*1*/
+	"TWISTED", /*2*/
+	"SLEEP PARALYSIS", /*3*/
+	"REGRETS", /*4*/
+	"CRIBBO", /*5*/
+	"SOLDIER OF LOVE", /*6*/
+	"DESIRE", /*7*/
+	"CONCEITED", /*8*/
+	"SWEET DREAMS", /*9*/
+	"ENDLESS", /*10*/
+	"INDIGO", /*11*/
+	"DABBINGTON CITY", /*12*/
+	"FALLEN ANGEL", /*13*/
+	"MJ", /*14*/
+	"FEAN", /*15*/
+	"I DON'T FEEL LIKE MYSELF", /*16*/
+	"ISOLATION ", /*17*/
+	"INDIFFERENT", /*18*/
+	"SACRIFICE", /*19*/
+	"I’M STILL HERE", /*20*/
+]
